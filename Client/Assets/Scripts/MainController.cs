d1c6a2d602f80f609833f8e4ced745c551c76e7b
k-hamada <k.hamada.1988@gmail.com>
2017-04-18 20:19:10 +0900

    [client] WebSocket コネクションを作成

--- Client/Assets/Scripts/MainController.cs
+++ Client/Assets/Scripts/MainController.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 
+using WebSocketSharp;
+
 public class MainController : MonoBehaviour
 {
+    WebSocket webSocket;    // WebSocketコネクション
+
     void Start()
     {
+        webSocket = new WebSocket("ws://localhost:5678");
+
+        webSocket.Connect();
     }
 
     void Update()
