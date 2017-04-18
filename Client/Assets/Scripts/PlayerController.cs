e91334b847816ba4de062b0844e85b2c37bacf24
k-hamada <k.hamada.1988@gmail.com>
2017-04-18 20:15:34 +0900

    [client] Initialize MainController

--- Client/Assets/Scripts/PlayerController.cs
+++ Client/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed = 10.0f; // 速度
+    public float speed = 10.0f; // 速度
 
-	void Start()
-	{
-	}
+    void Start()
+    {
+    }
 
-	// 固定フレームレートで呼び出されるハンドラ
-	void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+    // 固定フレームレートで呼び出されるハンドラ
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		var rb = GetComponent<Rigidbody>();
+        var rb = GetComponent<Rigidbody>();
 
-		// 速度の設定
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce(movement * speed);
-	}
+        // 速度の設定
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
 }
