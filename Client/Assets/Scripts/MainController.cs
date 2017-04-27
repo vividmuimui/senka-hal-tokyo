4b74d048d6ceb3009cc23343eb2831146191705c
Ryota Kanai <rkanai@aiming-inc.com>
2017-04-27 16:49:00 +0900

    [client] 各種イベントハンドラを追加

--- Client/Assets/Scripts/MainController.cs
+++ Client/Assets/Scripts/MainController.cs
@@ -10,6 +10,30 @@ public class MainController : MonoBehaviour
     {
         webSocket = new WebSocket("ws://localhost:5678");
 
+        // コネクション確立したときのハンドラ
+        webSocket.OnOpen += (sender, eventArgs) =>
+        {
+            Debug.Log("WebSocket Opened");
+        };
+
+        // エラーが発生したときのハンドラ
+        webSocket.OnError += (sender, eventArgs) =>
+        {
+            Debug.Log("WebSocket Error Message: " + eventArgs.Message);
+        };
+
+        // コネクションを閉じたときのハンドラ
+        webSocket.OnClose += (sender, eventArgs) =>
+        {
+            Debug.Log("WebSocket Closed");
+        };
+
+        // メッセージを受信したときのハンドラ
+        webSocket.OnMessage += (sender, eventArgs) =>
+        {
+            Debug.Log("WebSocket Message: " + eventArgs.Data);
+        };
+
         webSocket.Connect();
     }
 
