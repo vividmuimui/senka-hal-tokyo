e0b9a0dae45a734b9bfa4aa3f8c02c9e9a1b888c
Ryota Kanai <rkanai@aiming-inc.com>
2017-06-15 13:20:38 +0900

    [server] ファイル分割

---
+++ Server/GameServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WebSocketSharp.Server;
+
+namespace WebSocketSample.Server
+{
+    public class GameServer
+    {
+        const string SERVICE_NAME = "/";
+        const ConsoleKey EXIT_KEY = ConsoleKey.Q;
+
+        public event Action OnUpdate = () => { };
+
+        WebSocketServer WebSocketServer;
+
+        public GameServer(string address)
+        {
+            WebSocketServer = new WebSocketServer(address);
+            WebSocketServer.AddWebSocketService<WebSocketSampleService>(SERVICE_NAME, () => new WebSocketSampleService(this));
+        }
+
+        public void RunForever()
+        {
+            WebSocketServer.Start();
+            Console.WriteLine("Game Server started.");
+
+            while (!IsInputtedExitKey())
+            {
+                OnUpdate();
+            }
+        }
+
+        bool IsInputtedExitKey()
+        {
+            if (!Console.KeyAvailable) { return false; }
+
+            switch (Console.ReadKey(true).Key)
+            {
+                default:
+                    Console.WriteLine("Enter " + EXIT_KEY + " to exit the game.");
+                    return false;
+
+                case EXIT_KEY:
+                    WebSocketServer.Stop();
+                    Console.WriteLine("Game Server terminated.");
+                    return true;
+            }
+        }
+    }
+}
