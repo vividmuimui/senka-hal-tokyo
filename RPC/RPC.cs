1ba6bcbc61c7247fcf158ba449c6c750cfa24337
Ryota Kanai <rkanai@aiming-inc.com>
2017-06-21 21:22:12 +0900

    [rpc] アイテム入手API追加

--- RPC/RPC.cs
+++ RPC/RPC.cs
@@ -175,4 +175,27 @@ namespace WebSocketSample.RPC
             this.Position = position;
         }
     }
+
+    [System.Serializable]
+    public class GetItem
+    {
+        public string Method = "get_item";
+        public GetItemPayload Payload;
+
+        public GetItem(GetItemPayload payload)
+        {
+            this.Payload = payload;
+        }
+    }
+
+    [System.Serializable]
+    public class GetItemPayload
+    {
+        public int ItemId;
+
+        public GetItemPayload(int itemId)
+        {
+            this.ItemId = itemId;
+        }
+    }
 }
