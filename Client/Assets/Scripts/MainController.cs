e20b256e58dd97916797a06c65901ba05c654e43
vividmuimui <tkoyama@aiming-inc.com>
2017-10-30 02:15:33 +0900

    [client] WebSocket コネクションを作成

--- Client/Assets/Scripts/MainController.cs
+++ Client/Assets/Scripts/MainController.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using WebSocketSharp;
 
 public class MainController : MonoBehaviour
 {
+    WebSocket webSocket;    // WebSocketコネクション
+
+    [SerializeField]
+    private string connectAddress;
+
     void Start()
     {
+        webSocket = new WebSocket(connectAddress);
+        webSocket.Connect();
     }
 
     void Update()
